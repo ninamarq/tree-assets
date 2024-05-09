75e34b4fbae85380c2cf9ddfc2dc0c2c7c3dc90c
Marina Marques <marina.marqueso@outlook.com>
2024-05-09 11:05:34 -0300

    feat: add setup mocks and start tanstack query config

---
+++ mocks/index.ts
@@ -0,0 +1,5 @@
+import { companiesMock } from "./companies";
+import { assetsMock } from "./assets";
+import { locationsMock } from "./locations";
+
+export { companiesMock, assetsMock, locationsMock };
